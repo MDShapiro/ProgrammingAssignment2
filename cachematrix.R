752f7c93309e12b13fbc64fa76c2c0d0df32f403
MDShapiro <shapiro.michaeld2@gmail.com>
2015-03-21 23:56:02 -0400

    Assignment submission
    Initial submission of programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list of functions for setting and obtaining
+## matrix values from memory.
+## cacheSolve computes the inverse of the matrix created by makeCacheMatrix
 
-## Write a short comment describing this function
+## makeCacheMatrix stores computed matrix values in memory for later retrieval
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(),...) {
+  m <- NULL
+  setMatrix <- function(y) {
+    message("setting cache")
+    x <<- y
+    m <<- NULL
+  }
+  getMatrix <- function() {
+    #message("getting cached data from mCM")
+    x
+  }
+  setInvMatrix <- function(matrix) {
+    message("setting inverse of matrix")
+    m <<- matrix
+  }
+  getInvMatrix <- function() {
+    message("getting inverse from cache")
+    m
+  }
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of matrix 'x' and stores the resulting matrix in the
+## makeCacheMatrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrix()
+
+  if(!is.null(m)) {
+    mti <<- m
+    im <- solve(m)
+    x$setInvMatrix(im)
+    return(im)
+  }
+  else {
+   x$getInvMatrix()    
+  }
 }
